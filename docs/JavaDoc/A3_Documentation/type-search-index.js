c7ad8d30797e4956c2cd26c477d979e7a4cd366e
shyyahcheang <47626271+shyyahcheang@users.noreply.github.com>
2019-12-18 22:59:45 +0800

    Add files via upload

---
+++ docs/JavaDoc/A3_Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"InsertNumber"},{"p":"<Unnamed>","l":"PalindromeNumber"},{"p":"<Unnamed>","l":"ReversedInt"}]
