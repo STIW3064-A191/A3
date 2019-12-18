9dd3f61b2ac86cca10b3fa85ede450f706c93ce9
MEE WAI <38762366+MeeWai@users.noreply.github.com>
2019-12-18 21:41:19 +0800

    Update InsertNumber.java

--- src/InsertNumber.java
+++ src/InsertNumber.java
@@ -4,8 +4,8 @@ public class InsertNumber {
     void Number(){
         Scanner scan=new Scanner(System.in);
         System.out.print("Input: "); //insert an integer number
-        int a=scan.nextInt();
+        int a=scan.nextInt(); //user insert the data
         ReversedInt reversedInt = new ReversedInt();
-        reversedInt.isPalindrome(a);
+        reversedInt.isPalindrome(a); //calling another class to check whether the input is palindrome or not
     }
 }
