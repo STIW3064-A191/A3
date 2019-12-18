366b24541b8bcc90315cd47d000430fab7a1a912
MEE WAI <38762366+MeeWai@users.noreply.github.com>
2019-12-18 21:40:27 +0800

    Update PalindromeNumber.java

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,5 +1,5 @@
 public class PalindromeNumber {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main class of the system
         InsertNumber insertNumber = new InsertNumber();
         insertNumber.Number();
     }
